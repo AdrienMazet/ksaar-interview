ea811e2969bd1ad3955901acea8838369167e61e
Adrien Mazet <adrien.mazet.56@gmail.com>
2022-04-12 13:55:34 +0200

    Function to get closest number to zero from array of numbers, and tests

---
+++ index.js
@@ -0,0 +1,23 @@
+const getClosestNumberToZero = (numbers) => {
+  if (!numbers || numbers.length === 0) return 0;
+
+  return numbers.reduce((closestNumberToZero, currentNumber) => {
+    if (Math.abs(currentNumber) === Math.abs(closestNumberToZero)) {
+      return Math.max(closestNumberToZero, currentNumber);
+    }
+
+    return Math.abs(currentNumber) < Math.abs(closestNumberToZero)
+      ? currentNumber
+      : closestNumberToZero;
+  });
+};
+
+console.log(getClosestNumberToZero(undefined) === 0);
+console.log(getClosestNumberToZero([]) === 0);
+console.log(getClosestNumberToZero([-1]) === -1);
+console.log(getClosestNumberToZero([0]) === 0);
+console.log(getClosestNumberToZero([1]) === 1);
+console.log(getClosestNumberToZero([35, 303, 1058]) === 35);
+console.log(getClosestNumberToZero([-35, -303, 1058, 35]) === 35);
+console.log(getClosestNumberToZero([35, 303, 1058, -34]) === -34);
+console.log(getClosestNumberToZero([-34, 0, 35, 303, 1058]) === 0);
